95b0658f73e53ac05394c5c626e44fa50e30e07a
Ville Kalliomäki <i@mail.v-k.fi>
2022-05-08 15:37:37 +0300

    More verbose scrips

--- containers/stolon-backup/backup.sh
+++ containers/stolon-backup/backup.sh
@@ -1,22 +1,29 @@
-#!/bin/bash
+#!/bin/sh
+
+echo "Starting backup."
 
 # Get secrets
 source "$SOURCE_ENV_FILE"
 
 # Mount the remote backup directory
-sshfs -p 23 "$REMOTE_USER"@"$REMOTE_HOST":"$REMOTE_PATH" /mnt/remote -o IdentityFile=/vault/secrets/id_rsa
+echo "Mounting ${REMOTE_USER}@${REMOTE_HOST}:${$REMOTE_PATH} at /mnt/remote."
+sshfs -p 23 "$REMOTE_USER"@"$REMOTE_HOST":"$REMOTE_PATH" /mnt/remote -o IdentityFile=/vault/secrets/id_rsa -v
 
 # Dump all databases and roles
+echo "Dumping Stolon databases."
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -U "$POSTGRES_USER" -h "$POSTGRES_HOST" > /tmp/backup/stolon_pg_dumpall.out
 
 # Create a new backup
+echo "Creating backup of the dump."
 borg create --stats /mnt/remote::stolon-dumpall-{now}
 
 # Delete temporary file
 rm /tmp/backup/stolon_pg_dumpall.out
 
 # Prune old backups
+echo "Pruning existing backups."
 borgbackup prune -v --keep-daily=14 --keep-weekly=8 --keep-monthly=12 /mnt/remote
 
 # Unmount remote host
+echo "Unmount /mnt/remote."
 umount /mnt/remote
--- containers/stolon-backup/entrypoint.sh
+++ containers/stolon-backup/entrypoint.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
+echo "Started container."
 
 /backup.sh
