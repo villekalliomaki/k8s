de005bebab5740333a5ff44d83bb815242d36e63
Ville Kalliomäki <i@mail.v-k.fi>
2022-05-11 17:31:31 +0300

    Fix quotes, install curl during container build

--- containers/stolon-backup/notify.sh
+++ containers/stolon-backup/notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Sending notification"
+echo "Sending notification."
 
 if [[ -z "${GOTIFY_TOKEN}" ]] || [[ -z "${GOTIFY_ENDPOINT}" ]]; then
     echo "GOTIFY_ENDPOINT or GOTIFY_TOKEN not set, skipping notification."
