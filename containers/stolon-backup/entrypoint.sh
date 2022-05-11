3b96b6740dd931be5fcf3317b3dc312104ef9b30
Ville Kalliomäki <i@mail.v-k.fi>
2022-05-11 17:16:45 +0300

    Remove list command

--- containers/stolon-backup/entrypoint.sh
+++ containers/stolon-backup/entrypoint.sh
@@ -8,11 +8,4 @@ chmod 700 ~/.ssh
 cp /vault/secrets/id_rsa ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
 
-if [ "$1" = "backup" ]; then
-    /backup.sh
-elif [ "$1" = "list" ]; then
-    /list.sh
-else
-    echo "Invalid command ${1}. Valid command are 'backup' and 'list'."
-    exit 1
-fi
+/backup.sh
