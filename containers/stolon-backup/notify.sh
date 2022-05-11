900710d7c573c5df98c933c91e6b5ebe2108e2f1
Ville Kalliomäki <i@mail.v-k.fi>
2022-05-11 19:44:51 +0300

    Verbose notification

--- containers/stolon-backup/notify.sh
+++ containers/stolon-backup/notify.sh
@@ -5,10 +5,16 @@ echo "Sending notification."
 if [[ -z "${GOTIFY_TOKEN}" ]] || [[ -z "${GOTIFY_ENDPOINT}" ]]; then
     echo "GOTIFY_ENDPOINT or GOTIFY_TOKEN not set, skipping notification."
 else
+    body='{"priority": 1, "title": "Stolon Backup Created", "message": '"$(jq -R -s '.' < /tmp/msg)"'}'
+
+    echo "\n$body\n"
+
+    cat /tmp/msg
+
     curl \
         -X POST \
         -H "X-Gotify-Key: ${GOTIFY_TOKEN}" \
         -H "Content-Type: application/json" \
-        -d '{"priority": 1, "title": "Stolon Backup Created", "message": '"$(jq -R -s '.' < /tmp/msg)"'}' \
+        -d "$body" \
         "${GOTIFY_ENDPOINT}/message"
 fi
