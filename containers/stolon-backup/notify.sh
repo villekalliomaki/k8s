606e7b33637fcd820636cbe11d4ed562a6197716
Ville Kalliomäki <i@mail.v-k.fi>
2022-05-31 18:54:25 +0300

    Update notification command

--- containers/stolon-backup/notify.sh
+++ containers/stolon-backup/notify.sh
@@ -5,16 +5,5 @@ echo "Sending notification."
 if [[ -z "${GOTIFY_TOKEN}" ]] || [[ -z "${GOTIFY_ENDPOINT}" ]]; then
     echo "GOTIFY_ENDPOINT or GOTIFY_TOKEN not set, skipping notification."
 else
-    body='{"priority": 1, "title": "Stolon Backup Created", "message": '"$(jq -R -s '.' < /tmp/msg)"'}'
-
-    echo "\n$body\n"
-
-    cat /tmp/msg
-
-    curl \
-        -X POST \
-        -H "X-Gotify-Key: ${GOTIFY_TOKEN}" \
-        -H "Content-Type: application/json" \
-        -d "$body" \
-        "${GOTIFY_ENDPOINT}/message"
+    curl -X POST "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=New Stolon backup created" -F "message=$(jq -R -s '.' < /tmp/msg)"
 fi
